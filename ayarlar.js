c5e294f2f179a5692bbd27d3e94d76da46cd84b4
XRongo <86964552+XRongo@users.noreply.github.com>
2024-06-30 03:25:10 +0300

    Vanity Role

---
+++ ayarlar.js
@@ -0,0 +1,12 @@
+module.exports = {
+    bot_token: "", // Botun tokeni
+    bot_status: "", // Botun durumunda yazacak yazı
+
+    url_rol_sistemi: {
+        durum: false, // true yaparsanız sistemi açar false yaparsanız kapatır
+        rol_id: "", // Duruma yazıyı yazınca verilecek rolün ID'si
+        sunucu_id: "", // Sistemi Kullanacağınız Sunucunun ID'si
+        kanal_id: "", // Log Kanalının ID'si
+        yazı: "" // Rolün verilmesi için duruma yazılması gereken yazı
+    }
+}
